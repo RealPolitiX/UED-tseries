3a3e025e68c9dd394d40360ee2f93ac24e094ba5
R. Patrick Xian <xrpatrick@gmail.com>
2017-06-29 17:40:15 +0200

    Update TMfr2car.m

--- TMfr2car.m
+++ TMfr2car.m
@@ -1,10 +1,12 @@
 function TM = TMfr2car(a,b,c,alf,bet,gam)
+    % Construct transform matrix from fractional to Cartesian coordinates
+    
+    % Calculate unit cell volume
+    v = sqrt(1 - cosd(alf)^2 - cosd(bet)^2 - cosd(gam)^2 + 2*cosd(alf)*cosd(bet)*cosd(gam));
 
-v = sqrt(1 - cosd(alf)^2 - cosd(bet)^2 - cosd(gam)^2 + 2*cosd(alf)*cosd(bet)*cosd(gam));
-
-% Transformation matrix from fractional coordinates to Cartesian coordinates
-TM = [a b*cosd(gam) c*cosd(bet);
-    0 b*sind(gam) (c*(cosd(alf) - cosd(bet)*cosd(gam)))/sind(gam);
-    0 0 (c*v)/sind(gam)];
+    % Transformation matrix
+    TM = [a b*cosd(gam) c*cosd(bet);
+        0 b*sind(gam) (c*(cosd(alf) - cosd(bet)*cosd(gam)))/sind(gam);
+        0 0 (c*v)/sind(gam)];
 
 end
