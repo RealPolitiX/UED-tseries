a46f1fa33c32ccafd40e4538c122944fc2c114b3
R. Patrick Xian <xrpatrick@gmail.com>
2017-06-29 17:34:26 +0200

    initial commit

---
+++ PixelSum.m
@@ -0,0 +1,27 @@
+function pcount = PixelSum(img,pxlx,pxly)
+    % Sum over all designated pixel values of the input image
+    %
+    % Input arguments
+    % img: image matrix
+    % pxlx, pxly: the x & y Cartesian coordinates of summand pixels
+    %
+    % Output argument
+    % pcount: integrated value of all input pixels
+
+    nx = length(pxlx);
+    ny = length(pxly);
+    
+    % Input size check
+    if nx == ny
+        % Initiate the pixel intensity count to 0
+        pcount = 0;
+        for i = 1:nx
+            pcount = pcount + img(pxlx(i),pxly(i));
+        end
+
+    else
+        print('Input coordinate vectors have different lengths!')
+        
+    end
+
+end
